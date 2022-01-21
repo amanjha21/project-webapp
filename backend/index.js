e219ee3e9755393077596ea21e7f1ac1118d2373
Aman Jha <aman.jha21.aj@gmail.com>
2022-01-21 14:08:27 +0545

    Home Route added

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const routes = require("./routes/index");
 
+//Express Parser
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+//Routes
 app.use("/", routes);
 
+// Home route
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome, This is App's Backend" });
+});
+
+//Server Start
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
