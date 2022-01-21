400828719c6755d9158eec39264c0225ffede41e
Aman Jha <aman.jha21.aj@gmail.com>
2022-01-21 13:54:00 +0545

    routes added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,16 @@
 const router = require("express").Router();
-router.use("/", (req, res) => {
-  res.send("hi, its working");
-});
+const posts = require("./posts/index");
+const organizations = require("./organizations/index");
+const users = require("./users/index");
+const teams = require("./teams/index");
+const reactions = require("./reactions/index");
+const notices = require("./notices/index");
+
+router.use("/post", posts);
+router.use("/organization", organizations);
+router.use("/user", users);
+router.use("/team", teams);
+router.use("/reaction", reactions);
+router.use("/notice", notices);
+
 module.exports = router;
