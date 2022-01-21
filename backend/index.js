c72b4aeacaef8f76cc674cfa74e83ccc1cd339e7
Aman Jha <aman.jha21.aj@gmail.com>
2022-01-21 12:50:16 +0545

    express parser added as middleware

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
+require("./helpers/dbconnect");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const routes = require("./routes/index");
 
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/", routes);
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
