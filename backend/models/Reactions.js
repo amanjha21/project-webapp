017fc4dfe3fa0e40a3c1e25fbd6a6bdfee325a49
Aman Jha <aman.jha21.aj@gmail.com>
2022-01-24 20:32:19 +0545

    timestamp added

--- backend/models/Reactions.js
+++ backend/models/Reactions.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
-const reactionSchema = new mongoose.Schema({
-  type: String,
-  comment: String,
-  post: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: "Posts",
+const reactionSchema = new mongoose.Schema(
+  {
+    type: String,
+    comment: String,
+    post: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Posts",
+    },
+    user: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Users",
+    },
   },
-  user: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: "Users",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 module.exports = mongoose.model("Reactions", reactionSchema);
