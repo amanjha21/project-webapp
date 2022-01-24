2bc3dc99fcce360027e9408c175b1814ba6103f7
Aman Jha <aman.jha21.aj@gmail.com>
2022-01-24 20:39:22 +0545

    get posts

--- backend/routes/posts/getPosts.js
+++ backend/routes/posts/getPosts.js
@@ -1,3 +1,25 @@
-module.exports = (req, res) => {
-  res.send("this is posts");
+const Schemas = require("../../models/index");
+const pipeline = require("../../helpers/pipeline");
+
+module.exports = async (req, res) => {
+  const userId = "61eaeee6ef856a79a71d19b9" || 0;
+  const page = parseInt(req.query.page);
+  const noOfPosts = parseInt(req.query.number);
+  try {
+    const post = await Schemas.Post.aggregate(
+      pipeline.posts(userId, page, noOfPosts)
+    );
+    if (post.length == 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Request",
+      });
+    }
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(404).json({
+      success: false,
+      message: err.message,
+    });
+  }
 };
