a247780df10f882877fb64bc77d861fccae18fcb
Aman Jha <aman.jha21.aj@gmail.com>
2022-01-25 17:49:38 +0545

    time stamp added to notices schema

--- backend/models/Notices.js
+++ backend/models/Notices.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
-const noticeSchema = new mongoose.Schema({
-  content: String,
-  image_link: [String],
-  user: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: "Users",
+const noticeSchema = new mongoose.Schema(
+  {
+    content: String,
+    image_link: [String],
+    user: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Users",
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 module.exports = mongoose.model("Notices", noticeSchema);
