490cb1237e747a3be7fbcbfcab4070543e2cae0a
vinamra0 <singh001vinamra@gmail.com>
2022-01-31 23:09:45 +0545

    add reaction added

--- backend/routes/reactions/addReaction.js
+++ backend/routes/reactions/addReaction.js
@@ -1,8 +1,31 @@
 const Schemas = require("../../models/index");
 module.exports = async (req, res) => {
     const type = req.body.type;
-    const comment = req.body.comment;
+    const comment = req.body.comment || "";
     const user = req.body.userId;
     const post = req.body.postId;
+    try {
 
-}
+        const reaction = await Schemas.Reaction({
+            type: type,
+            comment: comment,
+            post: post,
+            user: user,
+
+        });
+        await reaction.save();
+        res.status(201).json({
+            success: true,
+            message: "Reaction added successfully"
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.status(404).json({
+            success: false,
+            message: err.message,
+        });
+
+    }
+
+};
