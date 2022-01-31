05e0fbe69afd9c7c122bf936db2da608296f8134
vinamra0 <singh001vinamra@gmail.com>
2022-01-31 23:09:35 +0545

    addReaction added

--- backend/routes/reactions/addReaction.js
+++ backend/routes/reactions/addReaction.js
@@ -1,5 +1,8 @@
 const Schemas = require("../../models/index");
 module.exports = async (req, res) => {
+    const type = req.body.type;
+    const comment = req.body.comment;
+    const user = req.body.userId;
+    const post = req.body.postId;
 
-    console.log("this is addReaction");
 }
