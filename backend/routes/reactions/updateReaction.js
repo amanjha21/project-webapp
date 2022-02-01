4f82d1aa15a18979752002babb78b2015d19ea04
vinamra0 <singh001vinamra@gmail.com>
2022-02-01 18:05:50 +0545

    update and delete reaction functions added

--- backend/routes/reactions/updateReaction.js
+++ backend/routes/reactions/updateReaction.js
@@ -1,5 +1,42 @@
 const Schemas = require("../../models/index");
 module.exports = async (req, res) => {
+    const type = req.body.type;
+    const user = req.body.userId;
+    const post = req.body.postId;
+    try {
+        const result = await Schemas.Reaction.findOne({
+            post: post,
+            user: user
+        }).exec();
 
-    console.log("this is updateReaction");
+        if (result.type === type) {
+            await Schemas.Reaction.deleteOne({
+                _id: result._id.toString()
+            }).exec();
+
+            if (result.deletedCount == 1) {
+                res.status(200).json({
+                    success: true,
+                    message: "Reaction deleted successfully!!"
+                });
+            }
+        } else {
+            result.type = type;
+            await result.save();
+            res.status(200).json({
+                success: true,
+                message: "Reaction updated successfully!!"
+            });
+
+        }
+
+    } catch (err) {
+        res.status(404).json({
+            success: false,
+            message: err.message
+        });
+
+    }
+
+
 }
