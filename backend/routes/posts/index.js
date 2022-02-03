027bdf6edb2232602e3c305b197c1a82e5ce1524
Aman Jha <aman.jha21.aj@gmail.com>
2022-02-03 18:59:28 +0545

    routes changed for post reactions

--- backend/routes/posts/index.js
+++ backend/routes/posts/index.js
@@ -8,6 +8,7 @@ const deletePostById = require("./deletePostById");
 const deletePostsByUserId = require("./deletePostsByUserId");
 const validation = require("../../middlewares/validation");
 const validationSchema = require("./@validationSchema");
+const reactions = require("./reactions/index");
 //get post route
 router.get("/", validation(validationSchema.getPosts, "query"), getPosts);
 //get post by postid route
@@ -41,4 +42,5 @@ router.delete(
   validation(validationSchema.deletePostsByUserIdValidation),
   deletePostsByUserId
 );
+router.use("/reaction", reactions);
 module.exports = router;
