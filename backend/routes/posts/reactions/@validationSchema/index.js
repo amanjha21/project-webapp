cc573f9720ff3ab35701f7cca6f1c88877990c85
vinamra0 <singh001vinamra@gmail.com>
2022-02-03 20:19:37 +0545

    reaction Validation created

---
+++ backend/routes/posts/reactions/@validationSchema/index.js
@@ -0,0 +1,19 @@
+const Joi = require("@hapi/joi");
+
+const addCommentValidation = Joi.object({
+    comment: Joi.string().required(),
+    user: Joi.string().lenghth(24).required(),
+    post: Joi.string().lenght(24).required()
+});
+
+const deleteCommentValidation = Joi.object({
+
+    userId: Joi.string().length(24).required(),
+    comment: Joi.string().required(),
+});
+
+const updateCommentValidation = Joi.object({
+    type: Joi.string().required,
+    userId: Joi.string().length(24).required(),
+    post: Joi.string().length(24).required(),
+})
--- backend/routes/posts/reactions/index.js
+++ backend/routes/posts/reactions/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 const addComment = require("./addComment");
 const deleteComment = require("./deleteComment");
 const updateReaction = require("./updateReaction");
+const validation = require("../../middlewares/validation");
+const validationSchema = require("./@validationSchema");
 
-router.post("/", addComment);
-router.post("/update", updateReaction);
-router.delete("/", deleteComment);
+router.post("/", validation(validationSchema.addCommentValidation), addComment);
+router.post("/update", validation(validationSchema.updateReactionValidation), updateReaction);
+router.delete("/", validation(validationSchema.deleteReactionValidation), deleteComment);
 module.exports = router;
