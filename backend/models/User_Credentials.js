ca0208e2c27f925f8b890e3b82c9fe6c94ff0612
Aman Jha <aman.jha21.aj@gmail.com>
2022-02-07 00:29:04 +0545

    token added

--- backend/models/User_Credentials.js
+++ backend/models/User_Credentials.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 const userCredSchema = new mongoose.Schema({
-        _id: {
-            type: mongoose.SchemaTypes.ObjectId,
-            ref: "Users",
-        },
-        email: String,
-        password: String,
-    }
-
-);
+  _id: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "Users",
+  },
+  email: String,
+  password: String,
+  token: String,
+});
 module.exports = mongoose.model("User_Credentials", userCredSchema);
