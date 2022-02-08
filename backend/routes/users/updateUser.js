47d057f9f45755b03972f66fd74c4c36ea69e722
vinamra0 <singh001vinamra@gmail.com>
2022-02-08 23:10:38 +0545

    changes made to updateUser route

--- backend/routes/users/updateUser.js
+++ backend/routes/users/updateUser.js
@@ -4,7 +4,7 @@ module.exports = async (req, res) => {
   const ip = req.header("x-forwarded-for") || req.connection.remoteAddress;
   const userId = req.params.id;
   const name = req.body.name;
-  const email_id = req.body.email;
+
   const teams = req.body.teams;
 
   //checking if the userId length is proper or not
@@ -15,7 +15,7 @@ module.exports = async (req, res) => {
     });
   }
   //checking if all the params exist
-  if (!name && !email_id && !teams) {
+  if (!name && !teams) {
     return res.status(400).json({
       success: false,
       message: "Nothing to update!!",
@@ -37,16 +37,7 @@ module.exports = async (req, res) => {
       user.name = name;
       await user.save();
     }
-    // checking if email_id exists and if it needs to be updated in the database
-    if (email_id && email_id !== user.email) {
-      user.email = email_id;
-      await user.save();
-      const userCredential = await Schemas.User_Credential.findOne({
-        _id: userId
-      });
-      userCredential.email = email_id;
-      await userCredential.save();
-    }
+
     // checking if teams exists and if it needs to be updated in the database
     if (teams && user.teams.includes(teams)) {
       user.teams.push(teams);
@@ -54,14 +45,14 @@ module.exports = async (req, res) => {
 
     }
     // checkin if the changes were made successfully
-    if (name == user.name || email_id == user.email || teams == user.teams) {
+    if (name == user.name || teams == user.teams) {
       res.status(200).json({
         success: true,
         message: "User updated",
       });
       logger({
         userId: user._id,
-        message: `User updated successfully by user with userId: ${user._id}`,
+        message: `User: ${user._id} updated successfully by user with userId: ${user._id}`,
         ip,
       });
     } else {
