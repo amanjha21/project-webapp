2aee667d1b7925e89b23212a9f7cd4b8147a9212
Aman Jha <aman.jha21.aj@gmail.com>
2022-02-08 22:27:10 +0545

    forget password route added

--- backend/routes/users/@validationSchema/index.js
+++ backend/routes/users/@validationSchema/index.js
@@ -1,44 +1,49 @@
 const Joi = require("@hapi/joi");
 
 const addUserValidation = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().regex(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/).required(),
-
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string()
+    .regex(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/)
+    .required(),
 });
 const updateUserPasswordValidation = Joi.object({
-    oldPassword: Joi.string().min(8).required(),
-    newPassword: Joi.string().min(8).required(),
-})
+  oldPassword: Joi.string().min(8).required(),
+  newPassword: Joi.string().min(8).required(),
+});
 
 const deleteUserValidation = Joi.object({
-    id: Joi.string().length(24).required(),
+  id: Joi.string().length(24).required(),
 });
 const updateUserValidation = Joi.object({
-    name: Joi.string().optional(),
-    email: Joi.string().email().optional(),
-    teams: Joi.array().items(Joi.string().length(24)),
-    newAdminId: Joi.string().email().optional(),
+  name: Joi.string().optional(),
+  email: Joi.string().email().optional(),
+  teams: Joi.array().items(Joi.string().length(24)),
+  newAdminId: Joi.string().email().optional(),
 });
 
 const getUserValidation = Joi.object({
-    id: Joi.string().length(24),
-
+  id: Joi.string().length(24),
 });
 
 const getUsersByTeamId = Joi.object({
-    teamId: Joi.string().length(24).required(),
-})
+  teamId: Joi.string().length(24).required(),
+});
 
 const getUsersByOrgainzationId = Joi.object({
-    organizationId: Joi.string().length(24).required(),
-})
+  organizationId: Joi.string().length(24).required(),
+});
+
+const resetUserPasswordValidation = Joi.object({
+  newPassword: Joi.string().min(8).required(),
+});
 module.exports = {
-    addUserValidation,
-    deleteUserValidation,
-    updateUserValidation,
-    getUsersByTeamId,
-    getUsersByOrgainzationId,
-    getUserValidation,
-    updateUserPasswordValidation,
+  addUserValidation,
+  deleteUserValidation,
+  updateUserValidation,
+  getUsersByTeamId,
+  getUsersByOrgainzationId,
+  getUserValidation,
+  updateUserPasswordValidation,
+  resetUserPasswordValidation,
 };
