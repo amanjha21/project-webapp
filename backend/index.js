35619beac7aa12f2620ea705a109a6459098651c
Aman Jha <aman.jha21.aj@gmail.com>
2022-02-09 18:09:56 +0545

    cors, helmet, response-time added

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,26 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const routes = require("./routes/index");
 
+const cors = require("cors");
+const helmet = require("helmet");
+const responseTime = require("response-time");
+
+// Set security headers
+app.use(helmet());
+
+// CORS
+app.use(cors());
+
+// Response time
+app.use(responseTime({ suffix: false }));
+
 //Express Parser
-app.use(express.urlencoded({
-  limit: "50mb",
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    limit: "50mb",
+    extended: true,
+  })
+);
 
 //Routes
 app.use("/", routes);
@@ -18,7 +33,7 @@ app.use("/", routes);
 // Home route
 app.get("/", (req, res) => {
   res.status(200).json({
-    message: "Welcome, This is App's Backend"
+    message: "Welcome, This is App's Backend",
   });
 });
 
