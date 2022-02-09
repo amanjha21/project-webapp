7284ca3210344403649510b26d17dc154a0af763
Aman Jha <aman.jha21.aj@gmail.com>
2022-02-10 00:51:41 +0545

    return user specific reactions if user has token

--- backend/routes/posts/getPostById.js
+++ backend/routes/posts/getPostById.js
@@ -1,9 +1,11 @@
 const Schemas = require("../../models/index");
 const pipeline = require("../../helpers/pipeline");
-
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 module.exports = async (req, res) => {
   const postId = req.params.id;
-  const userId = "61eaeee6ef856a79a71d19b9";
+  const token = req.header("Authorization").split(" ")[1] || "";
+  let userId = "";
   if (postId.length != 24) {
     return res.status(400).json({
       success: false,
@@ -11,6 +13,16 @@ module.exports = async (req, res) => {
     });
   }
   try {
+    //if token exists validate and extract userid
+    if (token) {
+      const user = jwt.verify(token, process.env.USER_TOKEN_SECRET);
+      const dbUser = await Schemas.User_Credential.findOne({ _id: user._id });
+      const dbToken = dbUser.token;
+
+      if (dbToken && dbToken == token) {
+        userId = dbUser._id;
+      }
+    }
     const post = await Schemas.Post.aggregate(
       pipeline.postById(postId, userId)
     );
