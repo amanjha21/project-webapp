5a6967c62fe72d0f3820b19bf2a04fc9824dcf0d
Aman Jha <aman.jha21.aj@gmail.com>
2022-02-17 00:33:45 +0545

    popup updated

--- frontend/src/components/Popup.js
+++ frontend/src/components/Popup.js
@@ -1,13 +1,15 @@
 import "./Popup.css";
 import { MdClose } from "react-icons/md";
 const Popup = ({ children, visible, setVisible }) => {
+  const closeHandler = (e) => {
+    if (e.target.classList.value === "popup-fullscreen-container") {
+      setVisible(false);
+    }
+  };
   return (
     <>
       {visible && (
-        <div
-          className="popup-fullscreen-container"
-          onClick={() => setVisible(false)}
-        >
+        <div className="popup-fullscreen-container" onClick={closeHandler}>
           <MdClose className="popup-close" onClick={() => setVisible(false)} />
           <div className="popup-container">{children}</div>
         </div>
