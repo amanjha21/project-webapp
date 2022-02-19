c9619eb985231d1f1283eebfacbbc77782ffa18a
Vinamra0 <singh001vinamra@gmail.com>
2022-02-19 23:32:00 +0545

    minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,17 +8,10 @@ import UserTeams from "./components/UserTeams/UserTeams";
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Navbar />
-      {/* <Post /> */}
-      <UserTeams />
-      {/* <Profile /> */}
-=======
       {/* <Navbar />
       <Post />
       <UserTeams />*/}
       <Profile />
->>>>>>> 446bf34b09f3bdbc96e83d047123aef328ad2298
       {/* <Login /> */}
     </div>
   );
