7bf3663e402759edbcde9660e3a29af97821e718
vaibhav <vaibhavkedia24@gmail.com>
2022-02-21 01:17:15 +0530

    added buttons in Member section

--- frontend/src/components/Profile/members/member.js
+++ frontend/src/components/Profile/members/member.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
 import Options from "../../Options";
-import { MdEdit, MdDelete } from "react-icons/md";
+import Confirmation from "../../Confirmation";
 
 const Member = ({ name, imgUrl, role = "member" }) => {
+  const [makeRole, setMakeRole] = useState(role);
+  const [showVisible, setShowVisible] = useState(false);
+
+  const makeAdminHandler = () => {
+    setMakeRole("admin");
+    setShowVisible(true);
+  };
+
+  const makeModeratorHandler = () => {
+    setMakeRole("moderator");
+  };
+
+  const makeMemberHandler = () => {
+    setMakeRole("member");
+  };
+
   return (
     <>
       <div className="member-list-container">
@@ -15,19 +32,31 @@ const Member = ({ name, imgUrl, role = "member" }) => {
                 </div>
                 <div className="member-meta">
                   <span className="name cursor">{name}</span>
-                  <span className="role">{role}</span>
+                  <span className="role">{makeRole}</span>
                 </div>
               </li>
             </ul>
-            <Options className="">
-              <button className="post-option">Admin</button>
-              <button className="post-option">Ad</button>
-              <button className="post-option">min</button>
-              <MdDelete className="post-option post-delete-option" />
+            <Options>
+              <button className="make-role-btn" onClick={makeAdminHandler}>
+                Make Admin
+              </button>
+              <button className="make-role-btn" onClick={makeModeratorHandler}>
+                Make moderator
+              </button>
+              <button className="make-role-btn" onClick={makeMemberHandler}>
+                Make Member
+              </button>
             </Options>
           </section>
         </div>
       </div>
+      <Confirmation
+        visible={showVisible}
+        setVisible={setShowVisible}
+        message={`Make ${name} admin and demote yourself?`}
+        option="Confirm"
+        onConfirm={() => console.log("Request Sent Successfully")}
+      />
     </>
   );
 };
