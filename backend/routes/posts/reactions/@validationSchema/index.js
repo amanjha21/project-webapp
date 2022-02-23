7c66030a80ddc6cdf0f85951b967915e8de2b4a3
Aman Jha <aman.jha21.aj@gmail.com>
2022-02-23 21:56:21 +0545

    type validation added

--- backend/routes/posts/reactions/@validationSchema/index.js
+++ backend/routes/posts/reactions/@validationSchema/index.js
@@ -1,24 +1,24 @@
 const Joi = require("@hapi/joi");
 
 const addCommentValidation = Joi.object({
-    comment: Joi.string().required(),
+  comment: Joi.string().required(),
 
-    postId: Joi.string().length(24).required()
+  postId: Joi.string().length(24).required(),
 });
 
 const deleteCommentValidation = Joi.object({
-    postId: Joi.string().length(24).required(),
-    comment: Joi.string().required(),
+  postId: Joi.string().length(24).required(),
+  comment: Joi.string().required(),
 });
 
 const updateCommentValidation = Joi.object({
-    type: Joi.string().required,
+  type: Joi.string().valid("like", "dislike").required(),
 
-    postId: Joi.string().length(24).required(),
-})
+  postId: Joi.string().length(24).required(),
+});
 
 module.exports = {
-    addCommentValidation,
-    deleteCommentValidation,
-    updateCommentValidation,
+  addCommentValidation,
+  deleteCommentValidation,
+  updateCommentValidation,
 };
