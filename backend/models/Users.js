d1b59fe51c3c7128cd4c5f6a6f830c94ed5090b8
Vinamra0 <singh001vinamra@gmail.com>
2022-02-26 00:41:44 +0545

    minor bug change

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
-  imageUrl: [String],
+  imageUrl: String,
   description: String,
   organization: {
     type: mongoose.SchemaTypes.ObjectId,
