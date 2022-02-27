3b342365f9016780a9b61bfc9d570319738f5823
Aman Jha <aman.jha21.aj@gmail.com>
2022-02-27 23:35:21 +0545

    image upload to cloudinary added

--- backend/routes/posts/addPost.js
+++ backend/routes/posts/addPost.js
@@ -1,10 +1,10 @@
 const Schemas = require("../../models/index");
-const uploader = require("../../helpers/uploader");
+const uploadImage = require("../../helpers/uploadImage");
 const logger = require("../../helpers/logger");
 module.exports = async (req, res) => {
   const userId = req.user._id;
   const content = req.body.content || "abcd";
-  const imageData = req.body.imageData;
+  const imageData = req.files;
   const ip = req.header("x-forwarded-for") || req.connection.remoteAddress;
   try {
     const post = new Schemas.Post({
@@ -15,13 +15,12 @@ module.exports = async (req, res) => {
     if (imageData) {
       let imageUrl = await Promise.all(
         imageData.map(async (data) => {
-          const url = await uploader(data);
+          const url = await uploadImage(data);
           return url;
         })
       );
       post.image_link = imageUrl;
     }
-
     const newPost = await post.save();
     logger({
       userId: userId,
