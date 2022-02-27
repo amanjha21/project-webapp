7e11f85d7820b2c5d21e8f311b9ee16f71c7d255
Aman Jha <aman.jha21.aj@gmail.com>
2022-02-27 23:37:07 +0545

    delete image with link

---
+++ backend/helpers/deleteImage.js
@@ -0,0 +1,19 @@
+const cloudinary = require("./cloudinary");
+const deleteImage = (imageUrl, imageUrlArray = []) => {
+  if (imageUrlArray.length > 0) {
+    imageUrlArray.map((image) => {
+      let fileName = image.split("/synoarx/")[1];
+      let publicId = "synoarx/" + fileName.substr(0, fileName.length - 4);
+      cloudinary.uploader.destroy(publicId, function (error, result) {
+        error && console.log(error);
+      });
+    });
+  } else {
+    let fileName = imageUrl.split("/synoarx/")[1];
+    let publicId = "synoarx/" + fileName.substr(0, fileName.length - 4);
+    cloudinary.uploader.destroy(publicId, function (error, result) {
+      error && console.log(error);
+    });
+  }
+};
+module.exports = deleteImage;
