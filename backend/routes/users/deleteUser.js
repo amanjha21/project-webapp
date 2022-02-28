df070a34de4524d7828d79abd8ddbbc3e6d51afb
Vinamra0 <singh001vinamra@gmail.com>
2022-02-28 21:35:23 +0545

    delete images added in delete user

--- backend/routes/users/deleteUser.js
+++ backend/routes/users/deleteUser.js
@@ -1,94 +1,101 @@
 const Schemas = require("../../models/index");
 pipeline = require("../../helpers/pipeline");
 const logger = require("../../helpers/logger");
+const deleteImage = require("../../helpers/deleteImage");
 module.exports = async (req, res) => {
   const ip = req.header("x-forwarded-for") || req.connection.remoteAddress;
   const userId = req.user._id;
-  const newAdminId = req.body.newadmin;
+  const newAdminEmail = req.body.newAdminEmail;
 
   try {
-    // const user = await Schemas.User.findOne({
-    //   _id: userId,
-    // }).exec();
-    // if (!user) {
-    //   res.status(404).json({
-    //     success: false,
-    //     message: "User doesnot exist!!",
-    //   });
-    // }
+    const user = await Schemas.User.findOne({
+      _id: userId,
+    }).exec();
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User doesnot exist!!",
+      });
+    }
     const userDetails = await Schemas.User.aggregate(
       pipeline.userDetails(userId)
     );
 
     // if user is admin and hasnt passed successor adminId
     // send err request as new admin email required
-    // const adminCheck = await Schemas.Team.find({
-    //   admin: userId,
-    // });
-    // if (adminCheck.length > 0) {
-    //   if (!newAdminId) {
-    //     return res.status(400).json({
-    //       success: false,
-    //       message: "new admin email required!!",
-    //     });
-    //   } else {
-    //     await Schemas.Team.findManyandUpdate(
-    //       {
-    //         admin: userId,
-    //       },
-    //       {
-    //         admin: newAdminId,
-    //       }
-    //     );
-    //   }
-    // }
+    const adminCheck = await Schemas.Team.find({
+      admin: userId,
+    });
+    if (adminCheck.length > 0) {
+      if (!newAdminEmail || newAdminEmail == user.email) {
+        return res.status(400).json({
+          success: false,
+          message: "new admin email required!!",
+        });
+      } else {
+        const newAdmin = await Schemas.User.findOne(
+          { email: newAdminEmail },
+          { _id: 1 }
+        );
+        console.log(newAdmin._id);
+        await Schemas.Team.updateMany(
+          {
+            admin: userId,
+          },
+          {
+            admin: newAdmin._id,
+          }
+        );
+      }
+    }
     // check if moderator exists
-    // const modCheck = await Schemas.Team.find({
-    //   moderator: {
-    //     $in: userId,
-    //   },
-    // });
+    const modCheck = await Schemas.Team.find({
+      moderator: {
+        $in: userId,
+      },
+    });
     //if true  change userId of Notice to admin's userId of that particular team member//
-    // if (modCheck.length > 0) {
-    //   const adminFetch = modCheck.map((obj) => obj.admin);
-    //   for (let i = 0; i < modCheck.length; i++) {
-    //     // find and update all the userId for the Notices sent by the mod to admin's id
-    //     await Schemas.Notice.findManyandUpdate(
-    //       {
-    //         user: userId,
-    //         team: modCheck[i]._id,
-    //       },
-    //       {
-    //         user: adminFetch[i],
-    //       }
-    //     );
+    if (modCheck.length > 0) {
+      const adminFetch = modCheck.map((obj) => obj.admin);
+      for (let i = 0; i < modCheck.length; i++) {
+        // find and update all the userId for the Notices sent by the mod to admin's id
+        await Schemas.Notice.updateMany(
+          {
+            user: userId,
+            team: modCheck[i]._id,
+          },
+          {
+            user: adminFetch[i],
+          }
+        );
 
-    //     await Schemas.Notice_Reaction.findManyandUpdate(
-    //       {
-    //         user: userId,
-    //         team: modCheck[i]._id,
-    //       },
-    //       {
-    //         user: adminFetch[i],
-    //       }
-    //     );
+        await Schemas.Notice_Reaction.updateMany(
+          {
+            user: userId,
+            team: modCheck[i]._id,
+          },
+          {
+            user: adminFetch[i],
+          }
+        );
 
-    //     const index = modCheck[i].moderator.indexOf(userId);
-    //     modCheck[i].moderator.splice(index, 1);
-    //   }
-    //   await modCheck.save();
-    // }
-    console.log(userDetails);
+        const index = modCheck[i].moderator.indexOf(userId);
+        modCheck[i].moderator.splice(index, 1);
+      }
+      await modCheck.save();
+    }
+
     // change the userId for notices and notice_reactions sent by old admin to the newly appointed one
 
     // delete notice_reaction by userId of the mod
 
-    // await deleteUser(userDetails[0]);
-    // logger({
-    //   email: user.email,
-    //   message: `User deleted successfully with userId: ${userId} `,
-    //   ip,
-    // });
+    await deleteUser(userDetails[0], user);
+
+    logger({
+      email: user.email,
+      message: `User deleted successfully with userId: ${userId} `,
+      ip,
+    });
 
     res.status(200).json({
       success: true,
@@ -101,7 +108,7 @@ module.exports = async (req, res) => {
     });
   }
 };
-const deleteUser = async (userDetails) => {
+const deleteUser = async (userDetails, user) => {
   try {
     //delete userReactions
     const userReactionArray = userDetails.reactions;
@@ -117,14 +124,20 @@ const deleteUser = async (userDetails) => {
         $in: userPostArray,
       },
     });
-    //delete user
-    const user = userDetails._id;
-    await Schemas.User.deleteOne({
-      _id: user,
+    userDetails.imageUrl.map((imageUrlArray) => {
+      deleteImage("", imageUrlArray);
     });
+    //delete user
+
+    await Schemas.User.deleteOne({
+      _id: userDetails._id,
+    });
+
+    deleteImage(user.imageUrl);
+
     //delete User Credentials
     await Schemas.User_Credential.deleteOne({
-      _id: user,
+      _id: userDetails._id,
     });
   } catch (err) {
     throw err;
