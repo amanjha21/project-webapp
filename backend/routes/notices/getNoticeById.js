da57b9c2cc5f7fb6dbde204b5eaeafffbf1375ce
Aman Jha <aman.jha21.aj@gmail.com>
2022-03-01 18:18:24 +0545

    refactor

--- backend/routes/notices/getNoticeById.js
+++ backend/routes/notices/getNoticeById.js
@@ -4,31 +4,23 @@ const auth = require("../../helpers/auth/index");
 module.exports = async (req, res) => {
   const noticeId = req.params.id;
   const userId = req.user._id;
-  if (noticeId.length != 24) {
-    return res.status(400).json({
-      success: false,
-      message: "Invalid Request",
-    });
-  }
   try {
+    if (noticeId.length != 24) {
+      throw new Error("Invalid Notice Id");
+    }
     const notice = await Schemas.Notice.aggregate(
       pipeline.noticeById(noticeId, userId)
     );
     if (notice.length == 0) {
-      return res.status(400).json({
-        success: false,
-        message: "Invalid Request",
-      });
+      throw new Error("Notice doesn't exist");
     }
     const hasAuth =
       (await auth.isMemberOfTeam(userId, notice[0].team)) ||
       (await auth.isModeratorOfTeam(userId, notice[0].team)) ||
       (await auth.isAdminOfTeam(userId, notice[0].team));
     if (!hasAuth)
-      return res.status(401).json({
-        success: false,
-        message: "you donot have auth to access this resource",
-      });
+      throw new Error("you donot have auth to access this resource");
+
     res.status(200).json(notice);
   } catch (err) {
     res.status(404).json({
