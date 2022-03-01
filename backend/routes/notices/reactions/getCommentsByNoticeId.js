4500ececbc8f7fedd4d774193e149e25e2d09eb0
Aman Jha <aman.jha21.aj@gmail.com>
2022-03-01 18:21:11 +0545

    refactor

--- backend/routes/notices/reactions/getCommentsByNoticeId.js
+++ backend/routes/notices/reactions/getCommentsByNoticeId.js
@@ -7,30 +7,21 @@ module.exports = async (req, res) => {
   const userId = req.user._id;
   const page = parseInt(req.query.page);
   const noOfNotices = parseInt(req.query.limit);
-  if (noticeId.length != 24) {
-    return res.status(400).json({
-      success: false,
-      message: "Notice/s doesn't exist",
-    });
-  }
   try {
+    if (noticeId.length != 24) {
+      throw new Error("Invalid Notice Id");
+    }
     //check if notice exists
     const notice = await Schemas.Notice.findOne({ _id: noticeId });
     if (!notice) {
-      return res.status(400).json({
-        success: false,
-        message: "Notice/s doesn't exist",
-      });
+      throw new Error("Notice/s doesn't exist");
     }
     const hasAuth =
       (await auth.isMemberOfTeam(userId, notice.team)) ||
       (await auth.isModeratorOfTeam(userId, notice.team)) ||
       (await auth.isAdminOfTeam(userId, notice.team));
     if (!hasAuth)
-      return res.status(401).json({
-        success: false,
-        message: "you donot have auth to access this resource",
-      });
+      throw new Error("you donot have auth to access this resource");
     //get comments
     const comments = await Schemas.Notice_Reaction.aggregate(
       pipeline.commentsByPostId(noticeId, userId, page, noOfNotices)
