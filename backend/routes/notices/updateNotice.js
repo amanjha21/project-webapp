ce8c9870282c5ce475004d5562021e9305b596a9
Aman Jha <aman.jha21.aj@gmail.com>
2022-03-01 18:19:01 +0545

    refactor and image handler

--- backend/routes/notices/updateNotice.js
+++ backend/routes/notices/updateNotice.js
@@ -1,43 +1,42 @@
 const Schemas = require("../../models/index");
-const uploader = require("../../helpers/uploader");
+const uploadImage = require("../../helpers/uploadImage");
+const deleteImage = require("../../helpers/deleteImage");
 const logger = require("../../helpers/logger");
 module.exports = async (req, res) => {
   const userId = req.user._id;
   const noticeId = req.body.noticeId;
   const ip = req.header("x-forwarded-for") || req.connection.remoteAddress;
   const content = req.body.content;
-  const imageData = req.body.imageData;
-
-  if (!content && !imageData) {
-    return res.status(403).json({
-      success: false,
-      message: "Nothing to update",
-    });
-  }
+  const imageData = req.files;
+  const deleteImageUrl = req.body.deleteImageUrl || false;
   try {
+    if (!content && imageData.length == 0 && !deleteImageUrl) {
+      throw new Error("Nothing to update");
+    }
     //check if this notice exists and belongs to this user
     const notice = await Schemas.Notice.findOne({ _id: noticeId }).exec();
-    if (!notice) {
-      return res.status(400).json({
-        success: false,
-        message: `Notice doesn't exist`,
-      });
+    if (!notice || notice.user != userId) {
+      throw new Error("Notice doesn't exist");
     }
-    if (notice.user != userId) {
-      return res.status(400).json({
-        success: false,
-        message: "Notice doesn't exist ",
-      });
-    }
+
     //update content if exists
-    if (content) {
+    if (content && content != notice.content) {
       notice.content = content;
     }
-    // update image if exixts
-    if (imageData) {
+    //if deleteImageUrl
+    if (deleteImageUrl) {
+      // delete old images
+      deleteImage("", notice.image_link);
+      notice.image_link = "";
+    }
+    // if image data exists
+    else if (imageData.length > 0) {
+      // delete old images
+      deleteImage("", notice.image_link);
+      //add new images
       let imageUrl = await Promise.all(
         imageData.map(async (data) => {
-          const url = await uploader(data);
+          const url = await uploadImage(data, `/team/${teamId}/notice-images/`);
           return url;
         })
       );
