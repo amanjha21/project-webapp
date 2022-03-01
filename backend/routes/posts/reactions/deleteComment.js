1818ef131abcf11c40cde092b6fa9bd2c624d1ad
Aman Jha <aman.jha21.aj@gmail.com>
2022-03-01 18:24:41 +0545

    comment changed to comment id and refactor

--- backend/routes/posts/reactions/deleteComment.js
+++ backend/routes/posts/reactions/deleteComment.js
@@ -1,42 +1,24 @@
 const Schemas = require("../../../models/index");
 module.exports = async (req, res) => {
   const userId = req.user._id;
-  const comment = req.body.comment;
-  const postId = req.body.postId;
+  const commentId = req.params.id;
   try {
     const reaction = await Schemas.Reaction.findOne({
       user: userId,
-      comment: comment,
-      post: postId
+      _id: commentId,
     }).exec();
     if (!reaction) {
-      return res.status(400).json({
-        success: false,
-        message: "Reaction doesn't exist",
-      });
+      throw new Error("Comment doesn't exist");
     }
-    if (reaction.user != userId) {
-      return res.status(403).json({
-        success: false,
-        message: "Reaction doesn't exist ",
-      });
-    }
-    const result = await Schemas.Reaction.deleteOne({
-      user: userId,
-      comment: comment,
-      post: postId
+
+    await Schemas.Reaction.deleteOne({
+      _id: commentId,
     }).exec();
-    if (result.deletedCount == 1) {
-      res.status(200).json({
-        success: true,
-        message: "Reaction deleted successfully",
-      });
-    } else {
-      res.status(200).json({
-        success: false,
-        message: "Failed to delete reaction",
-      });
-    }
+
+    res.status(200).json({
+      success: true,
+      message: "Comment deleted successfully",
+    });
   } catch (err) {
     return res.status(404).json({
       successs: false,
