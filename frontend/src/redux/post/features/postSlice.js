1e4bb5866d5dc4ffd7e0246f542babc61758741b
Aman Jha <aman.jha21.aj@gmail.com>
2022-03-03 22:22:26 +0545

    post slice redux init

---
+++ frontend/src/redux/post/features/postSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoading: false,
+  data: {},
+  error: "",
+};
+
+export const postSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {
+    setPosts: (state, action) => {
+      state.data = action.payload;
+    },
+    setLoading: (state, action) => {
+      state.isLoading = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { setPosts, setError, setLoading } = postSlice.actions;
+export default postSlice.reducer;
