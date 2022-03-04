18bc01628db9af8c7235aa229b7c00e9e5944d65
Aman Jha <aman.jha21.aj@gmail.com>
2022-03-04 22:17:10 +0545

    comment and reaction reducer added

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postReducer from "./post/features/postSlice";
+import postCommentReducer from "./post/features/commentSlice";
+import postReactionReducer from "./post/features/reactionSlice";
 
 const store = configureStore({
   reducer: {
     posts: postReducer,
+    postComments: postCommentReducer,
+    postReactions: postReactionReducer,
   },
 });
 
