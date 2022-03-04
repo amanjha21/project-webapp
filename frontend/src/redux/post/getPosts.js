b896a4d2fbe32cab28540df62e525d6309dd8fe2
Aman Jha <aman.jha21.aj@gmail.com>
2022-03-04 22:17:49 +0545

    auth header added to get post

--- frontend/src/redux/post/getPosts.js
+++ frontend/src/redux/post/getPosts.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { authHeader } from "../../helpers/authHeader";
 import { SERVER_ENDPOINT } from "../../helpers/Constants";
 import { setPosts, setError, setLoading } from "./features/postSlice";
 
@@ -9,7 +10,8 @@ const getPosts =
       dispatch(setError(""));
       dispatch(setLoading(true));
       const response = await axios.get(
-        `${SERVER_ENDPOINT}/post?page=${page}&limit=${limit}`
+        `${SERVER_ENDPOINT}/post?page=${page}&limit=${limit}`,
+        { headers: authHeader() }
       );
       dispatch(setLoading(false));
       dispatch(setPosts(response.data));
