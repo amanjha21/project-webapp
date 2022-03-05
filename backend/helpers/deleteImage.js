7e8299a4b3cf9b9e4e882266f75fec0880130941
Aman Jha <aman.jha21.aj@gmail.com>
2022-03-05 12:16:01 +0545

    filter empty string

--- backend/helpers/deleteImage.js
+++ backend/helpers/deleteImage.js
@@ -2,10 +2,14 @@ const cloudinary = require("./cloudinary");
 const deleteImage = (imageUrl, imageUrlArray = []) => {
   if (imageUrlArray.length > 0) {
     let publicIdArray = [];
-    imageUrlArray.map((image) => {
-      let fileName = image.split("/synoarx/")[1];
-      publicIdArray.push("synoarx/" + fileName.substr(0, fileName.length - 4));
-    });
+    imageUrlArray
+      .filter((image) => image !== "")
+      .map((image) => {
+        let fileName = image.split("/synoarx/")[1];
+        publicIdArray.push(
+          "synoarx/" + fileName.substr(0, fileName.length - 4)
+        );
+      });
     if (publicIdArray.length > 0) {
       cloudinary.api.delete_resources(publicIdArray, function (error, result) {
         error && console.log(error);
