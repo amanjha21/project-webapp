3addf64e6bb4a7c6bf47c6eb7d4e14027cc97017
Vinamra0 <singh001vinamra@gmail.com>
2022-03-06 15:08:04 +0545

    added exports

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,14 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import postReducer from "./post/features/postSlice";
 import postCommentReducer from "./post/features/commentSlice";
 import postReactionReducer from "./post/features/reactionSlice";
-import teamListReducer from "./userTeams/features/teamListSlice";
+import currentUserReducer from "./userTeams/features/currentUserSlice";
 
 const store = configureStore({
   reducer: {
     posts: postReducer,
     postComments: postCommentReducer,
     postReactions: postReactionReducer,
-    teamList: teamListReducer,
+    currentUser: currentUserReducer,
   },
 });
 
