0d09d192011a85c17c33f77695b016505ea39924
Vinamra0 <singh001vinamra@gmail.com>
2022-03-06 23:55:38 +0545

    added actual actions names in the drop down

--- frontend/src/components/Navbar/UserSettings.jsx
+++ frontend/src/components/Navbar/UserSettings.jsx
@@ -1,15 +1,43 @@
+import { useState } from "react";
+import Popup from "../Popup";
+import EditProfile from "../Profile/EditProfile/EditProfile";
+import { logout } from "../../auth";
 const UserSettings = () => {
-  const options = ["First Option", "Second Option", "LogOut"];
+  const [showEditProfile, setShowEditProfile] = useState(false);
+  const logoutHandler = (e) => {
+    e.preventDefault();
+    logout();
+  };
+  const editProfileHandler = (e) => {
+    e.preventDefault();
+    setShowEditProfile(true);
+    console.log("profileHandler");
+  };
+  const options = [
+    { name: "Edit Profile", clickHandler: editProfileHandler },
+    {
+      name: "Logout",
+      clickHandler: logoutHandler,
+    },
+  ];
   return (
-    <div className="user-settings-container rounded-corner">
-      {options &&
-        options.map((option, i) => (
-          <div className="user-settings-option" key={i}>
-            {" "}
-            {option}
-          </div>
-        ))}
-    </div>
+    <>
+      <div className="user-settings-container rounded-corner">
+        {options &&
+          options.map((option, i) => (
+            <div
+              className="user-settings-option"
+              key={i}
+              onClick={option.clickHandler}
+            >
+              {option.name}
+            </div>
+          ))}
+      </div>
+      <Popup visible={showEditProfile} setVisible={setShowEditProfile}>
+        <EditProfile />
+      </Popup>
+    </>
   );
 };
 
