bd433c6a5bbcb490c4727e65d7134f53919b3351
Aman Jha <aman.jha21.aj@gmail.com>
2022-03-09 23:53:55 +0545

    notices, comment and reaction added

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,6 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import postReducer from "./post/features/postSlice";
 import postCommentReducer from "./post/features/commentSlice";
 import postReactionReducer from "./post/features/reactionSlice";
+import noticeReducer from "./notice/features/noticeSlice";
+import noticeCommentReducer from "./notice/features/commentSlice";
+import noticeReactionReducer from "./notice/features/reactionSlice";
 import currentUserReducer from "./userTeams/features/currentUserSlice";
 import userProfileReducer from "./UserProfile/features/userSlice";
 import teamProfileReducer from "./TeamProfile/features/teamSlice";
@@ -11,6 +14,9 @@ const store = configureStore({
     posts: postReducer,
     postComments: postCommentReducer,
     postReactions: postReactionReducer,
+    notices: noticeReducer,
+    noticeComments: noticeCommentReducer,
+    noticeReactions: noticeReactionReducer,
     currentUser: currentUserReducer,
     userProfile: userProfileReducer,
     teamProfile: teamProfileReducer,
