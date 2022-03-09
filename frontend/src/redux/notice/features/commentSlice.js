7aa6af05cf78652635230660bbdadd954db4718f
Aman Jha <aman.jha21.aj@gmail.com>
2022-03-09 23:57:28 +0545

    comment by notice id

---
+++ frontend/src/redux/notice/features/commentSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoading: false,
+  data: [],
+  error: "",
+};
+
+export const commentSlice = createSlice({
+  name: "comments",
+  initialState,
+  reducers: {
+    setComments: (state, action) => {
+      state.data = [
+        ...state.data,
+        { ...action.payload.data[0], noticeId: action.payload.id },
+      ];
+    },
+    setLoading: (state, action) => {
+      state.isLoading = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { setComments, setError, setLoading } = commentSlice.actions;
+export default commentSlice.reducer;
