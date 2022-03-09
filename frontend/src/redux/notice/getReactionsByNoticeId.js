79f3d48db4576bcc41740e23de7d5b54e9247b60
Aman Jha <aman.jha21.aj@gmail.com>
2022-03-09 23:56:02 +0545

    get reaction by notice id

---
+++ frontend/src/redux/notice/getReactionsByNoticeId.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { authHeader } from "../../helpers/authHeader";
+import { SERVER_ENDPOINT } from "../../helpers/Constants";
+import { setCurrentUser } from "../userTeams/features/currentUserSlice";
+import { setReactions, setError, setLoading } from "./features/reactionSlice";
+
+const getReactionsByNoticeId = (id) => async (dispatch) => {
+  try {
+    dispatch(setError(""));
+    dispatch(setLoading(true));
+    const response = await axios.get(
+      `${SERVER_ENDPOINT}/notice/reaction/${id}`,
+      {
+        headers: authHeader(),
+      }
+    );
+    dispatch(setLoading(false));
+    dispatch(setReactions({ data: response.data, id }));
+  } catch (err) {
+    const errorResponse = err?.response?.data?.message || err.message || "";
+    dispatch(setLoading(false));
+    dispatch(setError(errorResponse));
+    if (errorResponse === "Access Denied") {
+      dispatch(setCurrentUser({}));
+    }
+  }
+};
+
+export { getReactionsByNoticeId };
