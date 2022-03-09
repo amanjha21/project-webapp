85e6ea6250575b6be40588b191a5692b5391d760
Aman Jha <aman.jha21.aj@gmail.com>
2022-03-09 23:59:15 +0545

    notice reaction

---
+++ frontend/src/redux/notice/features/reactionSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoading: false,
+  data: [],
+  error: "",
+};
+
+export const reactionSlice = createSlice({
+  name: "reactions",
+  initialState,
+  reducers: {
+    setReactions: (state, action) => {
+      state.data = [
+        ...state.data,
+        { reactions: action.payload.data, noticeId: action.payload.id },
+      ];
+    },
+    setLoading: (state, action) => {
+      state.isLoading = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { setReactions, setError, setLoading } = reactionSlice.actions;
+export default reactionSlice.reducer;
