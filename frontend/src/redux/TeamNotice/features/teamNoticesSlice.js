074b47a629a62101b3c14ca0bd7815e2932faf6a
vaibhav kedia <vaibhavkedia24@gmail.com>
2022-03-10 10:55:43 +0530

    created teamNoticesSlice

---
+++ frontend/src/redux/TeamNotice/features/teamNoticesSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isLoading: false,
+  data: [],
+  error: "",
+};
+
+export const teamNoticesSlice = createSlice({
+  name: "teamNotices",
+  initialState,
+  reducers: {
+    setTeamNotices: (state, action) => {
+      state.data = [...state.data, ...action.payload];
+    },
+    setLoading: (state, action) => {
+      state.isLoading = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { setTeamNotices, setLoading, setError } =
+  teamNoticesSlice.actions;
+export default teamNoticesSlice.reducer;
