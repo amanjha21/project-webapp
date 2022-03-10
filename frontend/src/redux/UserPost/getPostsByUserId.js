cfcace631956f8e3c04428b35e9a56baec16d5e1
vaibhav kedia <vaibhavkedia24@gmail.com>
2022-03-10 10:56:33 +0530

    fetched getPostsById from backend

---
+++ frontend/src/redux/UserPost/getPostsByUserId.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { authHeader } from "../../helpers/authHeader";
+import { SERVER_ENDPOINT } from "../../helpers/Constants";
+import { setUserPosts, setLoading, setError } from "../UserPost/features";
+
+const getPostsByUserId =
+  (userId, page = 1, limit = 10) =>
+  async (dispatch) => {
+    try {
+      dispatch(setError(""));
+      dispatch(setLoading(true));
+      const response = await axios.get(
+        `${SERVER_ENDPOINT}/post/user/${userId}?page=${page}&limit=${limit}`,
+        {
+          headers: authHeader(),
+        }
+      );
+      dispatch(setLoading(false));
+      dispatch(setUserPosts(response.data));
+    } catch (error) {
+      const errorResponse =
+        error?.response?.data?.message || error.message || "";
+
+      dispatch(setLoading(false));
+      dispatch(setError(errorResponse));
+    }
+  };
+
+export { getPostsByUserId };
