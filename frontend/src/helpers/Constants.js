0d2ff37d4e1ccf0e6033367725bd0de7b4eec1bc
Aman Jha <67479739+amanjha21@users.noreply.github.com>
2022-12-14 21:42:40 +0545

    Update Constants.js

--- frontend/src/helpers/Constants.js
+++ frontend/src/helpers/Constants.js
@@ -1,4 +1,4 @@
-export const SERVER_ENDPOINT = "https://synoarx-backend.onrender.com/";
+export const SERVER_ENDPOINT = "https://synoarx-backend.onrender.com";
 export const defaultTeamNotFoundImgUrl =
   "https://heatherchristenaschmidt.files.wordpress.com/2011/09/facebook_no_profile_pic2-jpg.gif";
 export const defaultUserNotFoundImgUrl =
